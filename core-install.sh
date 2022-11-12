4a7ec92796d6d6d75e4315eefe6dca87b073bb34
AKaisanlahti <aki.kaisanlahti@gmail.com>
2022-11-12 11:59:03 +0200

    Update core-install.sh

--- core-install.sh
+++ core-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt install git -y
+sudo apt-add-repository ppa:ansible/ansible
 sudo apt install ansible -y
+sudo add-apt-repository ppa:phoerious/keepassxc
 sudo apt install keepassxc -y
